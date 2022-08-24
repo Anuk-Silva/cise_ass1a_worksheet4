5e0adfa38bed62d834f54232169b62de3a9b8c2f
Anuk Silva <vwv9667@autuni.ac.nz>
2022-08-24 12:34:40 +1200

    Updated the index.css Styling, App.js and index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
+import { Route, NavLink, BrowserRouter as Router, Redirect } from "react-router-dom";
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article";
-// import NotFoundPage from "./pages/404";
+import NotFoundPage from "./pages/404";
 
 const App = () => {
   return (
@@ -12,22 +12,16 @@ const App = () => {
       <div>
         <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
         <ul className="header">
-          <li>
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/SEPractice">Select the Practice</NavLink>
-          </li>
-          <li>
-            <NavLink to="/SubmitArticle">Submit an Article</NavLink>
-          </li>
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
         </ul>
         <div className="content">
           <Route exact path="/" component={Home} />
           <Route path="/SEPractice" component={SEPractice} />
           <Route path="/SubmitArticle" component={SubmitArticle} />
+          <Route exact path="/404" component={NotFoundPage} />
+          <Redirect to="/404" />
         </div>
       </div>
     </Router>
